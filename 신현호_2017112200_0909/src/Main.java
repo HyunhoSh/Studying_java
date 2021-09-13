ac09b222c329df78315f6033f747ff95b48217af
HyunhoSb <hhshin9879@gmail.com>
2021-09-14 00:33:20 +0900

    1st week

---
+++ 신현호_2017112200_0909/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Input: ");
+		
+		int a,b,c;
+		
+		Scanner sc = new Scanner(System.in); 
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		System.out.println("Output: "+c+ " "+b+" "+a);
+		
+	}
+
+}
